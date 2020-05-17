cc4bf4621e045fac64bdb48b8d0b457541815fce
kaigikinyua <kaigikinyua@gmail.com>
2020-05-17 23:56:13 +0300

    topnav and sidenav done

--- static/js/UI.js
+++ static/js/UI.js
@@ -11,3 +11,27 @@ function hide(id){
     elem.style.display="none"
 }
 */
+
+function showSideNav(){
+    var sideNav=document.getElementById('sideNav');
+    sideNav.classList.remove('hide_Right');
+    sideNav.classList.add('anim_reveal_fromRight');
+    sideNav.classList.add('sideNavShow');
+    showCover()
+}
+function hideSideNav(){
+    var sideNav=document.getElementById('sideNav');
+    sideNav.classList.add('hide_Right');
+    sideNav.classList.remove('sideNavShow');
+    sideNav.classList.remove('anim_reveal_fromRight');
+    hideCover();
+}
+
+function showCover(){
+    var cover=document.getElementById('cover')
+    cover.style.display="block";
+}
+function hideCover(){
+    var cover=document.getElementById('cover')
+    cover.style.display="none";
+}
