63fde5ce1a6823a0fca2c9eafd9465859aeb4554
kaigikinyua <kaigikinyua@gmail.com>
2020-05-24 22:22:25 +0300

    alignment done now for the images and project documentation

---
+++ static/js/loading.js
@@ -0,0 +1,24 @@
+window.onload=()=>{
+    var load=document.getElementById('loading')
+    load.style.display="none"
+}
+loadingText()
+function  loadingText(){
+    var messages=['Hello There','Just setting up ....','... 1 more sec']
+    var messagePanel=document.getElementById('emojiLoad')
+    messages.forEach(message=>{
+        var m=document.createElement('p')
+        m.classList.add("emojiMessage")
+        m.innerHTML=message
+        messagePanel.appendChild(m)
+    })
+    setTimeout(criticalLoading,13000)
+}
+function criticalLoading(){
+    var messages=['#128517;','#128513;']
+    var emoji=document.getElementById('emoji')
+    messages.forEach(message=>{
+        emoji.innerHTML="&"+message
+    })
+
+}
