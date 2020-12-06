f25d903d58813d9e811ba9181567c62b1729a65f
kaigikinyua <kaigikinyua@gmail.com>
2020-12-06 19:53:39 +0300

    renovation

--- static/js/gd.js
+++ static/js/gd.js
@@ -2,23 +2,20 @@ const debug = true
 var baseUrl = "http://localhost:5500/";
 //var baseUrl="http://127.0.0.1:5500/";
 var projects = []
-
 //project images
 var currProjectImages = []
 var currImageNum = 0
 
-if (debug == false) {
+if (debug == false){
     baseUrl = "https://kaigikinyua.github.io/StaticPortfolio/"
 }
 
 new Vue({
     el: "#app",
     data: {
-        covers: fetchGD
+        covers: fetchGD,
     }
 })
-
-
 function fetchGD() {
     var data = fetch(baseUrl + "gdprojects.json")
         .then(res => res.json())
