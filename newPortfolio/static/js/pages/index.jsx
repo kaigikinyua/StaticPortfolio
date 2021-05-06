5833a5941afbcbd7ae7fe33a0b26efe6a3977f72
kaigikinyua <kaigikinyua@gmail.com>
2021-05-06 14:59:29 +0300

    reordering css and scss

--- newPortfolio/static/js/pages/index.jsx
+++ newPortfolio/static/js/pages/index.jsx
@@ -4,11 +4,14 @@ class Slides extends React.Component{
     }
     render(){
         var slides=this.props.slides.map((s,index)=>{
-            return <Slide slide={s} key={index.toString()}/>
+            return <Slide slide={s} key={index.toString()} id={index.toString()}/>
         })
         return (
-            <div className="slides">
-                {slides}
+            <div>
+                <div className="slides">
+                    {slides}
+                </div>
+                {<SlideControll slides={this.props.slides}/>}
             </div>
         )
     }
@@ -20,7 +23,7 @@ class Slide extends React.Component{
     }
     render(){
         return(
-            <div className="slide">
+            <div className="slide" id={this.props.id}>
                 <div className="card_text">    
                     <h1 className="superTitle">{this.props.slide.title}</h1>
                     <p>{this.props.slide.short}</p>
@@ -35,6 +38,22 @@ class Slide extends React.Component{
         )
     }
 }
+class SlideControll extends React.Component{
+    constructor(props,context){
+        super(props,context)
+    }
+    render(){
+        var buttons=this.props.slides.map((slide,index)=>{
+            return <a href={"#"+index}><div className='sliderButton'></div></a>
+        });
+        return(
+            <div className='slideControll'>
+                {buttons}
+            </div>
+        )
+    }
+}
+
 const headerSlides=[
     {title:"Web Developer",short:"Web development is my main professional focus.",ctActionLink:"./projects",ctActionTtitle:"My Projects",img:"./static/images/"},
     {title:"3D Graphic Designer",short:"3D Graphic Designer",ctActionLink:"./projects",ctActionTtitle:"My Projects",img:"./static/images/"},
